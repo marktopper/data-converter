ffcc960466ea5909bba880cc70c079c0b290d481
Mark Topper <mark@ulties.com>
2016-09-24 11:05:32 +0200

    Add currency code for countries

--- app/Converters/Models/Country.php
+++ app/Converters/Models/Country.php
@@ -4,10 +4,25 @@ namespace App\Converters\Models;
 
 class Country extends Model
 {
+    protected static $currencies;
+
     protected $fillable = ['code', 'name'];
+
+    protected $appends = ['currency'];
 
     public function getCodeAttribute()
     {
         return strtolower($this->attributes['code']);
+    }
+
+    public function getCurrencyAttribute()
+    {
+        if (is_null(static::$currencies)) {
+            static::$currencies = json_decode(file_get_contents(storage_path('app/data/currencies.json')), true);
+        }
+
+        $code = strtoupper($this->attributes['code']);
+
+        return strtolower(static::$currencies[$code]);
     }
 }
